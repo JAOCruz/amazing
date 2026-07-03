bbf9e344f38cefa07dbadcf00ed0fa5aa4f81e13
Jay <jay@amazingprosthetic.com>
2026-07-03 16:21:30 +0000

    fix(auth): force verified SendGrid sender on password reset emails

--- addons/custom_manufacturing_dashboard/models/res_users.py
+++ addons/custom_manufacturing_dashboard/models/res_users.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import contextlib
 import logging
-from odoo import models, fields, api
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
@@ -232,3 +234,79 @@ class ResUsersCustom(models.Model):
         acceptable_levels = workload_levels.get(max_workload, ['available'])
 
         return employees.filtered(lambda e: e.workload_status in acceptable_levels)
+
+    # ============================================
+    # PASSWORD RESET: USE VERIFIED SENDER
+    # ============================================
+
+    def _action_reset_password(self, signup_type="reset"):
+        """Override to force the verified SendGrid sender as From address."""
+        if self.env.context.get('install_mode') or self.env.context.get('import_file'):
+            return
+        if self.filtered(lambda user: not user.active):
+            raise UserError(_("You cannot perform this action on an archived user."))
+
+        create_mode = bool(self.env.context.get('create_user'))
+        self.mapped('partner_id').signup_prepare(signup_type=signup_type)
+
+        account_created_template = None
+        if create_mode:
+            account_created_template = self.env.ref('auth_signup.set_password_email', raise_if_not_found=False)
+            if account_created_template and account_created_template._name != 'mail.template':
+                _logger.error("Wrong set password template %r", account_created_template)
+                return
+
+        Icp = self.env['ir.config_parameter'].sudo()
+        verified_email = Icp.get_param('mail.default.from') or 'no-reply@amazingprosthetics.do'
+        reply_to = Icp.get_param('mail.catchall.alias') and Icp.get_param('mail.catchall.domain') and \
+            f"{Icp.get_param('mail.catchall.alias')}@{Icp.get_param('mail.catchall.domain')}" or \
+            'Amazinggstd@gmail.com'
+
+        email_values = {
+            'email_cc': False,
+            'auto_delete': True,
+            'message_type': 'user_notification',
+            'recipient_ids': [],
+            'partner_ids': [],
+            'scheduled_date': False,
+            'email_from': verified_email,
+            'reply_to': reply_to,
+        }
+
+        for user in self:
+            if not user.email:
+                raise UserError(_("Cannot send email: user %s has no email address.", user.name))
+            email_values['email_to'] = user.email
+            with contextlib.closing(self.env.cr.savepoint()):
+                if account_created_template:
+                    account_created_template.send_mail(
+                        user.id, force_send=True,
+                        raise_exception=True, email_values=email_values)
+                else:
+                    user_lang = user.lang or self.env.lang or 'en_US'
+                    body = self.env['mail.render.mixin'].with_context(lang=user_lang)._render_template(
+                        self.env.ref('auth_signup.reset_password_email'),
+                        model='res.users', res_ids=user.ids,
+                        engine='qweb_view', options={'post_process': True})[user.id]
+                    mail = self.env['mail.mail'].sudo().create({
+                        'subject': self.with_context(lang=user_lang).env._('Password reset'),
+                        'body_html': body,
+                        **email_values,
+                    })
+                    mail.send()
+            if signup_type == 'reset':
+                _logger.info("Password reset email sent for user <%s> to <%s>", user.login, user.email)
+                message = _('A reset password link was send by email')
+            else:
+                _logger.info("Signup email sent for user <%s> to <%s>", user.login, user.email)
+                message = _('A signup link was send by email')
+
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': 'Notification',
+                'message': message,
+                'sticky': False,
+            }
+        }
